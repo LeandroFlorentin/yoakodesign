1896ef4f01e97c119fe6814df348f27b22c0af0d
Maximiliano Failla <faillamaximiliano@gmail.com>
2024-03-19 18:31:52 -0300

    shadow nav

--- src/components/SectionVideos/SectionVideos.jsx
+++ src/components/SectionVideos/SectionVideos.jsx
@@ -3,20 +3,24 @@ const SectionVideos = ({ title, images = [] }) => {
     <div className="mt-3">
       <h6 className="fs-40 text-light fw-light overflow-hidden">{title}</h6>
       <div className="row d-flex justify-content-between ms-1 me-1">
-        {
-          images.map((value, key) => {
-            return (
-              <div key={key} className="col-12 col-md-6 ps-0">
-                <video autoPlay loop src={value.src} className="tamano-video col-12 col-md-11 mt-4 rounded-4" style={{ background: "black" }}>
-                  <source />
-                </video>
-              </div>
-            )
-          })
-        }
+        {images.map((value, key) => {
+          return (
+            <div key={key} className="col-12 col-md-6 ps-0">
+              <video
+                autoPlay
+                loop
+                src={value.src}
+                className="tamano-video col-12 col-md-11 mt-4 rounded-4"
+                style={{ background: "black" }}
+              >
+                <source />
+              </video>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SectionVideos;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -6,10 +6,22 @@ import { useMediaQuery } from "react-responsive";
 const Work = ({ reference }) => {
   const mediaLanding = useMediaQuery({ query: "(max-width:500px)" });
   return (
-    <div ref={reference} className={`h-auto background-work ${mediaLanding ? "p-5" : "p-6"} pt-5 pb-5 m-0 rounded-150`}>
-      <h3 className='fs-48 text-light overflow-hidden fw-bold'>Video edition</h3>
-      <p className='text-light m-0'>Videos of  ~15, 20 minutes. Shorts, reels, tiktok.</p>
-      <p className='text-light'>Edition focused on viewer retention & thumbnails that seek the public’s attention.</p>
+    <div
+      ref={reference}
+      className={`h-auto background-work ${
+        mediaLanding ? "p-5" : "p-6"
+      } pt-5 pb-5 m-0 rounded-150`}
+    >
+      <h3 className="fs-48 text-light overflow-hidden fw-bold">
+        Video edition
+      </h3>
+      <p className="text-light m-0">
+        Videos of ~15, 20 minutes. Shorts, reels, tiktok.
+      </p>
+      <p className="text-light">
+        Edition focused on viewer retention & thumbnails that seek the public’s
+        attention.
+      </p>
       <div>
         <SectionVideos title="Gaming" images={[1, 2]} />
         <SectionVideos title="Educational" images={[1, 2]} />
@@ -20,21 +32,32 @@ const Work = ({ reference }) => {
       </div>
       <div className="pt-5">
         <div className="row m-0">
-          <h3 className="col-12 fs-48 text-light overflow-hidden fw-bold">Design</h3>
+          <h3 className="col-12 fs-48 text-light overflow-hidden fw-bold">
+            Design
+          </h3>
           <div className="col-12">
-            <p className="col-12 col-md-8 col-sm-12 text-light fs-24">Find out how i helped these brands to seek the most approachable result to perfection on every design aspect, on every detail. </p>
+            <p className="col-12 col-md-8 col-sm-12 text-light fs-24">
+              Find out how i helped these brands to seek the most approachable
+              result to perfection on every design aspect, on every detail.{" "}
+            </p>
           </div>
           <div className="col-12">
-            <p className="col-12 col-md-8 col-sm-12 text-light fs-24">The projects under this title are pieces that search that level of quality, that tell a story, and communicate the brand’s desire through graphic and practical designing.</p>
+            <p className="col-12 col-md-8 col-sm-12 text-light fs-24">
+              The projects under this title are pieces that search that level of
+              quality, that tell a story, and communicate the brand’s desire
+              through graphic and practical designing.
+            </p>
           </div>
-          <button className='btn btn-light col-12 col-md-4 col-lg-3 fs-4 rounded-pill fw-semibold mt-5'>Contact me</button>
+          <button className="btn btn-light col-12 col-md-4 col-lg-3 fs-4 rounded-pill fw-semibold mt-5">
+            Contact me
+          </button>
           <div className="pt-5">
             <Card />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Work;
