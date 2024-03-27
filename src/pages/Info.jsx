e3b5424db0cec049a0c86c7b1d996571d71b87ca
Maximiliano Failla <faillamaximiliano@gmail.com>
2024-03-27 16:41:31 -0300

    section about

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,50 +1,75 @@
 import React from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/svg/logo.svg";
 import Profile from "../assets/about-profile.png";
-
+import be from "../assets/svg/behance.svg";
+import lnk from "../assets/svg/linkedin.svg";
+import ig from "../assets/svg/instagram.svg";
+import arrow from "../assets/svg/arrow-up-right.svg";
+import flag from "../assets/flag.png";
+import CV from "/CV-MARTORANOPEROZZI.pdf?url";
 const Info = ({ reference }) => {
   return (
     <div ref={reference} className="ht-100">
-      <div className="container d-flex text-light mt-120px">
-        {/* About */}
-        <div className="d-flex flex-column p-2">
-          <h4 className="info-title">INFO</h4>
-          <h6 className="aboutme-title">About me.</h6>
-          <div>
-            <p className="jm-text">I’m Joaquín Martorano, from Argentina! 🇦🇷</p>
-            {/* Cambiar el AR por la bandera arg */}
-            <p className="aboutme-txt">
-              My passion about design started when i was just a kid, at 10 years
-              old. I started designing banners, thumbnails and intros for videos
-              on Youtube,{" "}
-              <span style={{ color: "#3EC372" }}>just for fun.</span>
-            </p>
-            <p className="aboutme-txt">
-              It was then when i started mastering my skills using editing and
-              design software, but had no design thinking until i got to
-              college.
-            </p>
-            <p className="aboutme-txt">
-              I studied Industrial design and now, with
-              <span style={{ color: "#3EC372" }}>23 years old</span>, I have a
-              lot of multidisciplinary experience to share and make value of.
-            </p>
+      <div className="container d-flex text-light mt-120px flex-column gap-7">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex flex-column">
+            <h4 className="info-title">INFO</h4>
+            <h6 className="aboutme-title">About me.</h6>
+            <div style={{ width: "661px" }}>
+              <br />
+
+              <p className="jm-text">
+                I’m Joaquín Martorano, from Argentina!{" "}
+                <img src={flag} className="flag" />
+              </p>
+              <br />
+              <p className="aboutme-txt">
+                My passion about design started when i was just a kid, at 10
+                years old. I started designing banners, thumbnails and intros
+                for videos on Youtube,{" "}
+                <span style={{ color: "#3EC372" }}>just for fun.</span>
+                <br />
+                <br />
+                It was then when i started mastering my skills using editing and
+                design software, but had no design thinking until i got to
+                college.
+                <br />
+                <br />I studied Industrial design and now, with
+                <span style={{ color: "#3EC372" }}> 23 years old</span>, I have
+                a lot of multidisciplinary experience to share and make value
+                of.
+              </p>
+            </div>
+          </div>
+          <div className="d-flex flex-column-reverse gap-6">
+            <img src={Profile} alt="profile" className="photo-about" />
+            <img src={Logo} alt="logo" className="logo-about" />
           </div>
         </div>
-        {/* Photo & Logo*/}
-        <div className="p-2">
-          <img src={Logo} alt="logo" className="logo-about" />
-          <img src={Profile} alt="profile" className="photo-about" />
+
+        <div className="d-flex justify-content-between">
+          <button className="btn-contact">
+            <span className="btn-contact-text">
+              <a href={CV} target="_blank" className="link">
+                CV RESUME <img src={arrow} />
+              </a>
+            </span>
+          </button>
+          <div className="d-flex layers">
+            <a href="https://www.behance.net/joaquinmartora1" target="_blank">
+              <img src={be} alt="be layer" className="icon-social" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/joaquinmartorano/"
+              target="_blank"
+            >
+              <img src={lnk} alt="linkedin layer" className="icon-social" />
+            </a>
+            <a href="https://www.instagram.com/jmartorano_/" target="_blank">
+              <img src={ig} alt="instagram layer" className="icon-social" />
+            </a>
+          </div>
         </div>
-      </div>
-      {/* Button & Icons */}
-      <div className="d-flex justify-content-between align-items-center">
-        {/* Button */}
-        <button className="btn-contact">
-          <span className="btn-contact-text">CV RESUME ↗️</span>
-        </button>
-        {/* Icons */}
-        <div>Icons Here</div>
       </div>
     </div>
   );
