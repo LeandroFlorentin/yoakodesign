a60423f4fb45c1b0ac6467f50ce10ec7bf7ef12b
Maximiliano Failla <faillamaximiliano@gmail.com>
2024-03-26 21:14:10 -0300

    Creating data to modularize

---
+++ src/helpers/thumbnails.js
@@ -0,0 +1,43 @@
+import t1Thumbnail from "../assets/thumbnails/t-1.png";
+import t2Thumbnail from "../assets/thumbnails/t-2.png";
+import t3Thumbnail from "../assets/thumbnails/t-3.png";
+import t4Thumbnail from "../assets/thumbnails/t-4.png";
+import t5Thumbnail from "../assets/thumbnails/t-5.png";
+import t6Thumbnail from "../assets/thumbnails/t-6.png";
+import t7Thumbnail from "../assets/thumbnails/t-7.png";
+import t8Thumbnail from "../assets/thumbnails/t-8.png";
+
+export const thumbnails = [
+  {
+    src: t1Thumbnail,
+    id: "t-1",
+  },
+  {
+    src: t2Thumbnail,
+    id: "t-2",
+  },
+  {
+    src: t3Thumbnail,
+    id: "t-3",
+  },
+  {
+    src: t4Thumbnail,
+    id: "t-4",
+  },
+  {
+    src: t5Thumbnail,
+    id: "t-5",
+  },
+  {
+    src: t6Thumbnail,
+    id: "t-6",
+  },
+  {
+    src: t7Thumbnail,
+    id: "t-7",
+  },
+  {
+    src: t8Thumbnail,
+    id: "t-8",
+  },
+];
