398411ad3d7c93af79c20edcfd134042eeb600ac
unknown <leandro.florentin@hotmail.com>
2024-04-01 03:33:34 -0300

    15vh

--- src/components/OffCanvasTop/OffCanvasTop.jsx
+++ src/components/OffCanvasTop/OffCanvasTop.jsx
@@ -1,7 +1,7 @@
 const OffCanvasTop = ({ children, title }) => {
   return (
-    <div className="offcanvas offcanvas-top b-black" tabIndex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-      <div className="offcanvas-body b-black border-bottom-modal flex-grow-0">
+    <div className="offcanvas offcanvas-top b-black h-15" tabIndex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+      <div className="offcanvas-body b-black border-bottom-modal">
         {children}
       </div>
     </div>
